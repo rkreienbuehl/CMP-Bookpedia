156d7303fa807504354c89fd4495d96131c225a3
rogerkreienbuehl <r.kreienbuehl@gmx.ch>
2024-12-08 13:22:16 +0100

    commit after following the course

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/KtorRemoteBookDataSource.kt
@@ -0,0 +1,40 @@
+package com.plcoding.bookpedia.book.data.network
+
+import com.plcoding.bookpedia.book.data.dto.BookWorkDto
+import com.plcoding.bookpedia.book.data.dto.SearchResponseDto
+import com.plcoding.bookpedia.core.data.safeCall
+import com.plcoding.bookpedia.core.domain.DataError
+import com.plcoding.bookpedia.core.domain.Result
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+
+private const val BASE_URL = "https://openlibrary.org"
+
+class KtorRemoteBookDataSource(
+    private val httpClient: HttpClient
+): RemoteBookDataSource {
+    override suspend fun searchBooks(
+        query: String,
+        resultLimit: Int?
+    ): Result<SearchResponseDto, DataError.Remote> {
+        return safeCall<SearchResponseDto> {
+            httpClient.get(
+                urlString = "$BASE_URL/search.json"
+            ) {
+                parameter("q", query)
+                parameter("limit", resultLimit)
+                parameter("language", "eng")
+                parameter("fields", "key,title,language,cover_i,author_key,author_name,cover_edition_key,first_publish_year,ratings_average,ratings_count,number_of_pages_median,edition_count")
+            }
+        }
+    }
+
+    override suspend fun getBookDeatils(bookWorkId: String): Result<BookWorkDto, DataError.Remote> {
+        return safeCall<BookWorkDto> {
+            httpClient.get(
+                urlString = "$BASE_URL/works/$bookWorkId.json"
+            )
+        }
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/RemoteBookDataSource.kt
@@ -0,0 +1,16 @@
+package com.plcoding.bookpedia.book.data.network
+
+import com.plcoding.bookpedia.book.data.dto.BookWorkDto
+import com.plcoding.bookpedia.book.data.dto.SearchResponseDto
+import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.core.domain.DataError
+import com.plcoding.bookpedia.core.domain.Result
+
+interface RemoteBookDataSource {
+    suspend fun searchBooks(
+        query: String,
+        resultLimit: Int? = null
+    ): Result<SearchResponseDto, DataError.Remote>
+
+    suspend fun getBookDeatils(bookWorkId: String): Result<BookWorkDto, DataError.Remote>
+}
